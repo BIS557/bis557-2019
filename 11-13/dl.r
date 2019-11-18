3c92b41ede3c66eb36808e90571547af7cb9e7e5
Michael Kane <kaneplusplus@gmail.com>
2019-11-18 11:47:40 -0500

    DL tuning.

--- 11-13/dl.r
+++ 11-13/dl.r
@@ -4,6 +4,7 @@ library(keras)
 #use_virtualenv("~/r-reticulate")
 
 dl_model <- keras_model_sequential() %>%
+  layer_dense(units = 4) %>%
   layer_dense(units = 3, activation = "softmax") 
 
 dl_model %>% compile(
@@ -14,10 +15,21 @@ dl_model %>% compile(
 form <- Species ~ . -1
 
 iris_x <- model.matrix(form, iris)
-iris_y <- to_categorical(iris$Species)
+iris_y <- to_categorical(as.numeric(iris$Species) - 1)
 
 dl_model %>% fit(
-  iris_x
-  iris_y
-  batch_size = 50,
+  iris_x,
+  iris_y,
+  batch_size = 150,
+  epoch = 1000,
   validation_split = 0.1)
+
+dl_acc <- 
+  apply(predict(dl_model, iris_x), 1, which.max) == as.numeric(iris$Species)
+sum(dl_acc) / length(dl_acc)
+
+library(e1071)
+
+svm_fit <- svm(form, iris)
+acc <- predict(svm_fit) == iris$Species
+sum(acc) / length(acc)
